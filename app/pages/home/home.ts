34f01ffd819e0d1ed9db43a77bf512a82e9762e6
czmj <clarazmjordan@gmail.com>
2017-01-28 22:16:53 +0000

    remove background color

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -40,28 +40,15 @@ export class HomePage {
   }
 
   ngAfterViewInit() {
-    // Either subscribe in controller or set in HTML
-    this.swingStack.throwin.subscribe((event: DragEvent) => {
-      event.target.style.background = '#ffffff';
-    });
 
     this.cards = [{email: ''}];
     this.addNewCards(1);
   }
 
 onItemMove(element, x, y, r) {
-  var color = '';
   var abs = Math.abs(x);
   let min = Math.trunc(Math.min(16*16 - abs, 16*16));
-  let hexCode = this.decimalToHex(min, 2);
 
-  if (x < 0) {
-    color = '#FF' + hexCode + hexCode;
-  } else {
-    color = '#' + hexCode + 'FF' + hexCode;
-  }
-
-  element.style.background = color;
   element.style['transform'] = `translate3d(0, 0, 0) translate(${x}px, ${y}px) rotate(${r}deg)`;
 }
 
@@ -79,17 +66,5 @@ addNewCards(count: number) {
       this.cards.push(val);
     }
   })
-}
-
-// http://stackoverflow.com/questions/57803/how-to-convert-decimal-to-hex-in-javascript
-decimalToHex(d, padding) {
-  var hex = Number(d).toString(16);
-  padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
-
-  while (hex.length < padding) {
-    hex = "0" + hex;
-  }
-
-  return hex;
 }
 }
